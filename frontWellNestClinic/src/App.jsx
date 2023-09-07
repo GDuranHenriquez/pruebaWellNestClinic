67e1220ae377b5b7da33b799c6e432f155b429a3
Giomy8 <giomarmesa@gmail.com>
2023-09-07 09:37:28 -0500

    Agregando a MyProfile GM

--- frontWellNestClinic/src/App.jsx
+++ frontWellNestClinic/src/App.jsx
@@ -1,14 +1,14 @@
 import LandingPage from './components/LandingPage/LandingPage';
-import HomePages from './pages/Home/Home'
+import HomePages from './pages/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import {Route, Routes, useLocation} from "react-router-dom";
 import CheckUser from './components/CheckUser/CheckUser'
 import Appoiment from './pages/Appointment/Appoiment';
 import MySchedulePage from './pages/mySchedule/MySchedulePage';
-import LoginPages from './pages/Login/LoginPages'
+import LoginPages from './pages/Login/LoginPages';
 import SignUp from './pages/SignUp/SignUp';
 import MyProfilePage from './pages/MyProfile/MyProfilePage';
-import './App.css'
+import './App.css';
 import MyPharmacyPage from './pages/pharmacy/pharmacyPage';
 
 
--- frontWellNestClinic/src/components/MyProfileComp/MyProfileComp.jsx
+++ frontWellNestClinic/src/components/MyProfileComp/MyProfileComp.jsx
@@ -1,19 +1,39 @@
-import  { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
-import {  useSelector } from 'react-redux';
 import defaultProfile from '../../assets/perfil.png';
-import style from './MyProfile.module.css'
+import style from './MyProfile.module.css';
 
 function MyProfileComp() {
-  const userClient = useSelector((state) => state.user)
-  const [newPassword, setNewPassword] = useState(''); 
-  const [confirmPassword, setConfirmPassword] = useState(''); 
+  const [isUploading, setIsUploading] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isChangingPassword, setIsChangingPassword] = useState(false);
+  const [currentAddress, setCurrentAddress] = useState('');
+  const [newAddress, setNewAddress] = useState('');
+  const [confirmAddress, setConfirmAddress] = useState('');
+  const [isChangingAddress, setIsChangingAddress] = useState(false);
+  const [currentContact, setCurrentContact] = useState('');
+  const [newContact, setNewContact] = useState('');
+  const [confirmContact, setConfirmContact] = useState('');
+  const [isChangingContact, setIsChangingContact] = useState(false);
 
-  useEffect(() => {
-    
-  }, []);
+  
+const takePhoto = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const videoElement = document.createElement('video');
+      document.body.appendChild(videoElement);
+      videoElement.srcObject = stream;
+      videoElement.play();
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+      alert('Unable to access camera.');
+    }
+  };
+  
 
-  const cambiarFotoDePerfil = () => {
+const changeProfilePicture = () => {
     const input = document.createElement('input');
     input.type = 'file';
     input.accept = 'image/*';
@@ -28,87 +48,117 @@ function MyProfileComp() {
         const formData = new FormData();
         formData.append('profilePicture', file);
 
-        const response = await axios.post('https://serverwellnestclinic.onrender.com/api/change-profile-picture', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-
-        updateProfilePicture(response.data.imageUrl);
+        const response = await axios.post(
+          'https://serverwellnestclinic.onrender.com/api/change-profile-picture',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        );
+        alert('Profile picture changed successfully.');
       } catch (error) {
-        console.error('Error al cambiar la foto de perfil:', error);
+        console.error('Error changing profile picture:', error);
+        alert('Unable to change profile picture.');
+      } finally {
+        setIsUploading(false);
       }
     });
 
     input.click();
   };
 
-  const cambiarContraseña = async () => {
-    if (!newPassword || !confirmPassword) {
-      alert('Por favor, complete todos los campos.');
+const changePassword = async () => {
+    if (!currentPassword || !newPassword || !confirmPassword) {
+      alert('Please fill in all fields.');
       return;
     }
 
     if (newPassword !== confirmPassword) {
-      alert('Las contraseñas no coinciden.');
+      alert('Passwords do not match.');
       return;
     }
 
     try {
-      const response = await axios.put('https://serverwellnestclinic.onrender.com/api/change-password', {
-        newPassword: newPassword,
-      });
-
-      updatePassword(response.data.newPassword);
-      alert('Contraseña cambiada exitosamente.');
+      const response = await axios.put(
+        'https://serverwellnestclinic.onrender.com/api/change-password',
+        {
+          currentPassword: currentPassword,
+          newPassword: newPassword,
+        }
+      );
+      alert('Password changed successfully.');
     } catch (error) {
-      console.error('Error al cambiar la contraseña:', error);
-      alert('No se pudo cambiar la contraseña.');
+      console.error('Error changing password:', error);
+      alert('Unable to change password.');
+    } finally {
+      setIsChangingPassword(false);
+      setCurrentPassword('');
+      setNewPassword('');
+      setConfirmPassword('');
     }
   };
 
-  const cambiarDireccion = async () => {
-    const newAddress = prompt('Ingrese la nueva dirección:');
+const changeAddress = async () => {
+    if (!currentAddress || !newAddress || !confirmAddress) {
+      alert('Please fill in all fields.');
+      return;
+    }
 
-    if (!newAddress) {
+    if (newAddress !== confirmAddress) {
+      alert('Addresses do not match.');
       return;
     }
 
     try {
-      const response = await axios.put('https://serverwellnestclinic.onrender.com/api/change-address', {
-        newAddress: newAddress,
-      });
-
-      updateAddress(response.data.newAddress);
-      alert('Dirección cambiada exitosamente.');
+      const response = await axios.put(
+        'https://serverwellnestclinic.onrender.com/api/change-address',
+        {
+          currentAddress: currentAddress,
+          newAddress: newAddress,
+        }
+      );
+      alert('Address changed successfully.');
     } catch (error) {
-      console.error('Error al cambiar la dirección:', error);
-      alert('No se pudo cambiar la dirección.');
+      console.error('Error changing address:', error);
+      alert('Unable to change address.');
+    } finally {
+      setIsChangingAddress(false);
+      setCurrentAddress('');
+      setNewAddress('');
+      setConfirmAddress('');
     }
-  }
+  };
 
-  const cambiarContacto = async () => {
-    const newContact = prompt('Ingrese el nuevo contacto:');
-
-    if (!newContact) {
+const changeContact = async () => {
+    if (!currentContact || !newContact || !confirmContact) {
+      alert('Please fill in all fields.');
       return;
     }
-
-    try {
-      const response = await axios.put('https://serverwellnestclinic.onrender.com/api/change-contact', {
-        newContact: newContact,
-      });
-
-      updateContact(response.data.newContact);
-      alert('Contacto cambiado exitosamente.');
-    } catch (error) {
-      console.error('Error al cambiar el contacto:', error);
-      alert('No se pudo cambiar el contacto.');
+  
+    if (newContact !== confirmContact) {
+      alert('Contacts do not match.');
+      return;
     }
-  }
+  
+    try {
+      alert('Contact changed successfully.');
+    } catch (error) {
+      console.error('Error changing contact:', error);
+      alert('Unable to change contact.');
+    } finally {
+      setIsChangingContact(false);
+      setCurrentContact('');
+      setNewContact('');
+      setConfirmContact('');
+    }
+  };
 
-  const cancelarMembresia = async () => {
-    const confirmation = window.confirm('¿Estás seguro de que deseas cancelar tu membresía? Esta acción es irreversible.');
+const cancelMembership = async () => {
+    const confirmation = window.confirm(
+      'Are you sure you want to cancel your membership? This action is irreversible.'
+    );
 
     if (!confirmation) {
       return;
@@ -116,46 +166,118 @@ function MyProfileComp() {
 
     try {
       await axios.post('https://serverwellnestclinic.onrender.com/api/cancel-membership');
-      cancelMembership();
-      alert('Membresía cancelada exitosamente.');
+      alert('Membership canceled successfully.');
     } catch (error) {
-      console.error('Error al cancelar la membresía:', error);
-      alert('No se pudo cancelar la membresía.');
+      console.error('Error canceling membership:', error);
+      alert('Unable to cancel membership.');
     }
   };
 
   return (
-    <div className={style.containerPerfil}>
-      <div className={style.dataPerfil}>
-        <h1 id={style.nombrePerfil}>{userClient.name} {userClient.lastName}</h1>
-        <img id={style.imgProfile} src={!userClient.imageUrl? defaultProfile : defaultProfile} alt="Foto de perfil" />
+    <div className={style.profileContainer}>
+      <div className={style.profileData}>
+        <h1 id={style.fullName}>{userClient.name} {userClient.lastName}</h1>
+        <img id={style.profileImage} src={!userClient.imageUrl ? defaultProfile : defaultProfile} alt="Profile" />
         <p>{userClient.email}</p>
         <p>DNI: {userClient.dni}</p>
-        <p>Tipo de DNI: {userClient.dniType}</p>
-        <p>Date of birth: {userClient.birthDate}</p>
+        <p>DNI Type: {userClient.dniType}</p>
+        <p>Date of Birth: {userClient.birthDate}</p>
         <p>Address: {userClient.address}</p>
         <p>Contact: {userClient.backupContact}</p>
-        <p>Membership status: {userClient.activePlan}</p>
-        <p>Next payment date: {userClient.nextPaymentDate}</p>
-      </div>
-      
-      <div className={style.accionsUsers}>
-        <button onClick={cambiarFotoDePerfil}>Change profile picture</button>
-      <div>
-        <input type="password" placeholder="Nueva Contraseña" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-        <input type="password" placeholder="Confirmar Contraseña" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-        <button onClick={cambiarContraseña}>Change password</button>
-      </div>
-      <button onClick={cancelarMembresia}>Cancel membership</button>
-      <button onClick={cambiarDireccion}>Change address</button>
-      <button onClick={cambiarContacto}>Change contact</button>
+        <p>Membership Status: {userClient.activePlan}</p>
+        <p>Next Payment Date: {userClient.nextPaymentDate}</p>
       </div>
 
-      
+      <div className={style.userActions}>
+        <h2>Configuration Options</h2>
+        <button onClick={changeProfilePicture}>
+        {isUploading ? 'Uploading...' : 'Change Profile Picture'}
+        </button>
+        <button onClick={takePhoto}>Take Photo</button>
+      </div>
+
+      <div className={style.userActions}>
+      <div>
+      <input
+      type="text"
+      placeholder="Current Contact"
+      value={currentContact}
+      onChange={(e) => setCurrentContact(e.target.value)}
+    />
+    <input
+      type="text"
+      placeholder="New Contact"
+      value={newContact}
+      onChange={(e) => setNewContact(e.target.value)}
+    />
+    <input
+      type="text"
+      placeholder="Confirm New Contact"
+      value={confirmContact}
+      onChange={(e) => setConfirmContact(e.target.value)}
+    />
+    <button onClick={changeContact}>
+      {isChangingContact ? 'Changing...' : 'Change Contact'}
+    </button>
+  </div>
+    </div>
+    <div className={style.userActions}>
+        <div>
+          <input
+            type="text"
+            placeholder="Current Address"
+            value={currentAddress}
+            onChange={(e) => setCurrentAddress(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="New Address"
+            value={newAddress}
+            onChange={(e) => setNewAddress(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Confirm New Address"
+            value={confirmAddress}
+            onChange={(e) => setConfirmAddress(e.target.value)}
+          />
+          <button onClick={changeAddress}>
+            {isChangingAddress ? 'Changing...' : 'Change Address'}
+          </button>
+        </div>
+    </div>
+
+    <div className={style.userActions}>
+    <div>
+    <input
+      type="text"
+      placeholder="Current Contact"
+      value={currentContact}
+      onChange={(e) => setCurrentContact(e.target.value)}
+    />
+    <input
+      type="text"
+      placeholder="New Contact"
+      value={newContact}
+      onChange={(e) => setNewContact(e.target.value)}
+    />
+    <input
+      type="text"
+      placeholder="Confirm New Contact"
+      value={confirmContact}
+      onChange={(e) => setConfirmContact(e.target.value)}
+    />
+    <button onClick={changeContact}>
+      {isChangingContact ? 'Changing...' : 'Change Contact'}
+    </button>
+    </div>
+    </div>
+
+    <div className={style.userActions}>
+    <button onClick={cancelMembership}>Cancel Membership</button>
+    </div>
     </div>
   );
 }
-
-
 
 export default MyProfileComp;
