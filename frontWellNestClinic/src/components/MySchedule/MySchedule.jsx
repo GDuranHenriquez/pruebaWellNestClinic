0888828d08a0bfe0e97bb25d95549a5007eccc59
MDPereyra22 <pereyra.dmatias@gmail.com>
2023-09-14 18:14:20 -0300

    MyScheudle: aplica delete y muestra citas canceladas. MP

--- frontWellNestClinic/src/components/MySchedule/MySchedule.jsx
+++ frontWellNestClinic/src/components/MySchedule/MySchedule.jsx
@@ -3,23 +3,30 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styles from './MySchedule.module.css';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const MySchedule = () => {
-    const [schedule, setSchedule] = useState([]);
-    const user = useSelector((state) => state.user)
+    const [openAppointments, setOpenAppointments] = useState([]);
+    const [canceledAppointments, setCanceledAppointments] = useState([]);
+    const user = useSelector((state) => state.user);
     const userId = user.id;
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchAppointments = async () => {
             try {
-
                 const response = await axios.get(`https://serverwellnestclinic.onrender.com/appointment/byUser/?userId=${userId}`);
                 if (Array.isArray(response.data)) {
-                    setSchedule(response.data);
-                } else {
-                    setSchedule([]);
-                }
 
+                    const open = response.data.filter(appointment => appointment.Status_Appointment.status === 'open');
+                    const canceled = response.data.filter(appointment => appointment.Status_Appointment.status === 'cancel');
+                    
+                    setOpenAppointments(open);
+                    setCanceledAppointments(canceled);
+                } else {
+                    setOpenAppointments([]);
+                    setCanceledAppointments([]);
+                }
             } catch (error) {
                 console.error('Error al obtener las citas médicas:', error);
             }
@@ -34,8 +41,10 @@ const MySchedule = () => {
         if (confirmDelete) {
             try {
                 await axios.delete(`https://serverwellnestclinic.onrender.com/appointment/${id}`);
-                const updatedSchedule = schedule.filter((item) => item.id !== id);
-                setSchedule(updatedSchedule);
+
+                const updatedCanceledAppointments = canceledAppointments.filter(item => item.id !== id);
+                setCanceledAppointments(updatedCanceledAppointments);
+                navigate("/home")
             } catch (error) {
                 console.error('Error al cancelar la cita médica:', error);
             }
@@ -45,46 +54,53 @@ const MySchedule = () => {
     return (
         <div className={styles.container}>
             <h2 className={styles.header}>My Schedule</h2>
-            {schedule.length === 0 ? (
+            {openAppointments.length === 0 && canceledAppointments.length === 0 ? (
                 <div className={styles.containerSection}>
                     <p className={styles.noAppointments}>You don't have any scheduled appointments</p>
                 </div>
             ) : (
-
                 <div className={styles.containerSection}>
-                    <ul className={styles.scheduleList}>
+                    {openAppointments.length > 0 && (
+                        <div>
+                            <h3>Open Appointments</h3>
+                            <ul className={styles.scheduleList}>
+                                {openAppointments.map(appointment => (
+                                    <li key={appointment.id} className={styles.appointment}>
+                                        <span className={styles.date}>{appointment.date}</span>
+                                        <span className={styles.time}>{appointment.startTime}</span>
+                                        <span className={styles.doctor}>
+                                            {`${appointment.Appointment_Doctor.name} ${appointment.Appointment_Doctor.lastName}`}
+                                        </span>
+                                        <span className={styles.specialty}>{appointment.Appointment_Speciality.name}</span>
+                                        <button
+                                            className={styles['cancel-button']}
+                                            onClick={() => handleCancelSchedule(appointment.id)}
+                                        >
+                                            Cancel appointment
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
 
-
-                        <li className={styles.appointment}>
-                            <span className={styles.date}>Date</span>
-                            <span className={styles.date}>Schedule</span>
-                            <span className={styles.date}>Doctor</span>
-                            <span className={styles.date}>Specialty</span>
-                            <span className={styles.date}></span>
-
-
-                        </li>
-
-                    </ul>
-                    <ul className={styles.scheduleList}>
-
-                        {schedule.map((appointment) => (
-                            <li key={appointment.id} className={styles.appointment}>
-                                <span className={styles.date}>{appointment.date}</span>
-                                <span className={styles.time}>{appointment.startTime}</span>
-                                <span className={styles.doctor}>
-                                    {`${appointment.Appointment_Doctor.name} ${appointment.Appointment_Doctor.lastName}`}
-                                </span>
-                                <span className={styles.specialty}>{appointment.Appointment_Doctor.specialty}</span>
-                                <button
-                                    className={styles['cancel-button']}
-                                    onClick={() => handleCancelSchedule(appointment.id)}
-                                >
-                                    Cancel appointment
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+                    {canceledAppointments.length > 0 && (
+                        <div>
+                            <h3>Canceled Appointments</h3>
+                            <ul className={styles.scheduleList}>
+                                {canceledAppointments.map(appointment => (
+                                    <li key={appointment.id} className={styles.appointment}>
+                                        <span className={styles.date}>{appointment.date}</span>
+                                        <span className={styles.time}>{appointment.startTime}</span>
+                                        <span className={styles.doctor}>
+                                            {`${appointment.Appointment_Doctor.name} ${appointment.Appointment_Doctor.lastName}`}
+                                        </span>
+                                        <span className={styles.specialty}>{appointment.Appointment_Speciality.name}</span>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             )}
 
