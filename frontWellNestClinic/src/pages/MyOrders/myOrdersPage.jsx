d3569411297e4892ca3a5fa768ec0cfef7fcc035
GDHenriquez <gregorioduranedrian123@gmail.com>
2023-09-21 02:56:20 -0400

    My profyle, pharmacy, bug fixing detailProduct GDuran Ines Francisco

--- frontWellNestClinic/src/pages/MyOrders/myOrdersPage.jsx
+++ frontWellNestClinic/src/pages/MyOrders/myOrdersPage.jsx
@@ -1,13 +1,13 @@
 import style from "../pharmacy/pharmacyPage.module.css";
 import BackGrounPharmacy from '../../components/Pharmacy/BackGrounPharmacy';
-import MyOrdersComp from '../../components/MyOrders/myOrdersComp';
+import MyOrdersComp from '../../components/MyOrders/MyOrdersComp';
 
 
-function ShoppingCartPage(){
+function MyOrdersPage(){
   return (<div className={style.MyPharmacyPage}>
     <BackGrounPharmacy></BackGrounPharmacy>
     <MyOrdersComp/>
   </div>)
 }
 
-export default ShoppingCartPage;
+export default MyOrdersPage;
